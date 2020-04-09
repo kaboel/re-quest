bc1eb010fe2de2d8b4a036ebdb1621d3d91a9fe7
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-09 20:16:57 +0700

    add statefull component

---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class StateFullComponent extends React.Component {
+  render() {
+    return <p> ini adalah Statefull Component</p>;
+  }
+}
+
+export default StateFullComponent;
