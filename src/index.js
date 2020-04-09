5f33e1c907b8399cff5cf4303af491ab66dd3db8
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-09 20:30:39 +0700

    dependency adjustment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./App";
+import BlogPost from "./container/BlogPost/BlogPost";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<BlogPost />, document.getElementById("content"));
