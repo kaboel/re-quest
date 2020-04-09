ef48bbe37bb0872214cfc06216e923347857ef33
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-09 20:13:16 +0700

    add get endpoint

---
+++ src/services/Get.js
@@ -0,0 +1,19 @@
+import { domainPath } from "./Config";
+
+const getAPI = (path) => {
+  const promise = new Promise((resolve, reject) => {
+    fetch(`${domainPath}/${path}`)
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          resolve(result);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+  });
+  return promise;
+};
+
+export default getAPI;
